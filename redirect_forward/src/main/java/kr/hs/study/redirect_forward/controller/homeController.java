3e2e978d03861ecb2209d8295d4ce0e7f0565b53
Chaeyoung Lim <rnldyal1027@naver.com>
2023-06-28 12:04:50 +0900

    Initial commit

---
+++ redirect_forward/src/main/java/kr/hs/study/redirect_forward/controller/homeController.java
@@ -0,0 +1,20 @@
+package kr.hs.study.redirect_forward.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController {
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+    @GetMapping("/redirect")
+    public String redirect(){
+        return  "redirect:result";
+    }
+    @GetMapping("result")
+    public String result(){
+        return "result";
+    }
+}
